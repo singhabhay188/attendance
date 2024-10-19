c55878bdff888b022de001c15fa56fd2e80c58da
Abhay Singh <singhchirag188@gmail.com>
2024-10-19 14:10:31 +0530

    renamed the folder to smallCase

---
+++ app/Student/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>this is student page</div>
+  )
+}
+
+export default page
--- app/Teacher/page.jsx
+++ app/Teacher/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const page = () => {
+const page = () => {
   return (
-    <div>techer</div>
+    <div>this is teacher page</div>
   )
 }
+
+export default page
